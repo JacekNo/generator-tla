7b3025f8aaae60a2da536f62fd5113dfbb215fbd
JacekNo <jacek.k.nowacki@gmail.com>
2026-01-05 11:12:33 +0100

    Naprawa ścieżek base w vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    glsl() // Dodajemy obsługę shaderów (masz to w package.json)
+  ],
+  // KLUCZOWE: Musi być nazwa Twojego repozytorium w slaszach
+  base: '/generator-tla/', 
 })
